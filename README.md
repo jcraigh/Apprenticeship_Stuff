45d715b0a8e08d6a6e4cd4ea759e92d8cdd04130
JJ <jacob.j.craighead@aexp.com>
2020-01-23 13:39:24 +0000

    Concurrency

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ out of the training but this file will contain it if it is to do with apprentice
 - [Annotations](src/training/ilovevideogames/Annotations)
 - [Video Games stuff](src/training/ilovevideogames/GameStuff/Main.java)
 - [Sorting Algorithms](src/training/ilovevideogames/SortingAlgorithims/Run_Counting.java)
+- [Concurrency](src/training/ilovevideogames/Concurrency)
---
+++ src/training/ilovevideogames/Concurrency/Concurrency.java
@@ -0,0 +1,57 @@
+package training.ilovevideogames.Concurrency;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Concurrency {
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+
+        for (int i = 0; i < 10_000; i++) {
+            Counter counterA = new Counter();
+            Counter counterB = new Counter();
+
+            CompletableFuture<Void> increment1 = CompletableFuture.runAsync(counterA::increment, pool);
+            CompletableFuture<Void> increment2 = CompletableFuture.runAsync(counterA::increment, pool);
+
+            CompletableFuture<Void> all = CompletableFuture.<Integer>allOf(increment1, increment2);
+            all.thenApply((v) -> {
+                if (counterA.get() != 2) {
+                    System.out.println("Incorrect counter value: " + Integer.toString(counterA.get()));
+                    System.out.println(counterB.get());
+                }
+
+                return null;
+            });
+        }
+
+        waitForThreadpoolShutdown(pool);
+    }
+
+    private static void waitForThreadpoolShutdown(ExecutorService pool) throws InterruptedException {
+        pool.shutdownNow();
+        if (!pool.awaitTermination(10, TimeUnit.SECONDS)) {
+            System.err.println("Pool did not complete within 10 seconds");
+            pool.shutdownNow();
+            if (!pool.awaitTermination(10, TimeUnit.SECONDS)) {
+                System.err.println("Pool did not terminate");
+            }
+        }
+    }
+
+    public static class Counter {
+        private AtomicInteger val = new AtomicInteger(0);
+
+        public synchronized void increment() {
+            val.incrementAndGet();
+        }
+
+        public synchronized int get() {
+            return val.get();
+        }
+    }
+}
